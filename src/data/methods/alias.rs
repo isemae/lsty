fdebf8fd6ce17dd44623809884a35d9150158865
isemae <foxlis97@naver.com>
2024-03-26 12:01:26 +0900

    Fix '\', '/' exceptions

--- src/data/methods/alias.rs
+++ src/data/methods/alias.rs
@@ -2,19 +2,25 @@ use crate::{
     cli::menu,
     data::{data_manager::DataManager, model::DataObject},
 };
+use std::process;
 
 impl DataManager {
     pub fn set_alias(&self, data: &mut DataObject, alias: String) {
-        println!(
-            "updating alias of the current directory rules from \n'{}' to '{}' (y/N)",
-            data.alias, alias
-        );
-        match menu::get_yn_input() {
-            true => {
-                println!("updated alias: {} -> {}", data.alias, alias);
-                data.alias = alias;
+        if alias.contains('/') || alias.contains('\\') {
+            eprintln!("[!] invalid alias: alias should not contain '/' or '\\'.");
+            process::exit(1);
+        } else {
+            println!(
+                "updating alias of the current directory rules from \n'{}' to '{}' (y/N)",
+                data.alias, alias
+            );
+            match menu::get_yn_input() {
+                true => {
+                    println!("updated alias: {} -> {}", data.alias, alias);
+                    data.alias = alias;
+                }
+                false => return,
             }
-            false => return,
         }
     }
 }
