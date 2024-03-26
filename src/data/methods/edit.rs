2dcdd04fe8a4f7c9e66e13e94111547323007217
isemae <foxlis97@naver.com>
2024-03-26 16:19:25 +0900

    Feature edit keyword from data

---
+++ src/data/methods/edit.rs
@@ -0,0 +1,34 @@
+use crate::{
+    cli::menu,
+    data::{data_manager::DataManager, model::DataModel},
+};
+use camino::Utf8PathBuf;
+use std::{env, process};
+
+impl DataManager {
+    pub fn edit_rule(&self, data: &mut DataModel, keyword: String, replacement: String) {
+        let source_path = Utf8PathBuf::from_path_buf(env::current_dir().unwrap_or_default())
+            .expect("valid Unicode path succeeded");
+        if let Some(obj) = data.data.iter_mut().find(|o| o.source == source_path) {
+            let targets = obj.targets.clone();
+            let target_path = targets
+                .get(&keyword)
+                .expect("found no target path for the key.");
+            println!(
+                "would you like to change keyword for the rule? {} -> {} (y/N)",
+                keyword, replacement
+            );
+
+            match menu::get_yn_input() {
+                true => {
+                    obj.targets.remove(&keyword);
+                    obj.targets.insert(replacement, target_path.to_string());
+                }
+                false => process::exit(1),
+            }
+        } else {
+            eprintln!("no such rule for the keyword");
+        }
+        self.save_json_data(&data).expect("");
+    }
+}
--- src/data/methods/mod.rs
+++ src/data/methods/mod.rs
@@ -1,5 +1,6 @@
 pub mod add;
 pub mod alias;
+pub mod edit;
 pub mod import;
 pub mod remove;
 pub mod rename;
