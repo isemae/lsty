8ce4f823f187936fa43b235a0ec91009fadb9314
isemae <foxlis97@naver.com>
2024-03-30 16:12:13 +0900

    Change migrate termion -> crossterm

--- src/cli/menu.rs
+++ src/cli/menu.rs
@@ -1,8 +1,11 @@
-extern crate crossterm;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::{
+    cursor,
+    event::{read, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+
 use std::io::{self, Write};
 use std::path::Path;
-use termion::{cursor, event::Key, input::TermRead};
 
 pub fn entry_symbol<T: AsRef<Path>>(entry: T) -> String {
     let entry = entry.as_ref();
@@ -22,55 +25,61 @@ pub fn get_yn_input() -> bool {
     enable_raw_mode().expect("Failed to enable raw mode");
     print_yn_prompt().expect("Failed to print prompt");
 
-    for key in io::stdin().keys() {
-        match key.unwrap() {
-            Key::Char('y') | Key::Char('Y') | Key::Char('ㅛ') => {
-                disable_raw_mode().expect("Failed to disable raw mode");
-                return true;
+    loop {
+        if let Ok(Event::Key(event)) = read() {
+            // execute!(std::io::stdout(), Clear(ClearType::All)).unwrap();
+            match event.code {
+                KeyCode::Char('y') | KeyCode::Char('Y') | KeyCode::Char('ㅛ') => {
+                    disable_raw_mode().expect("Failed to disable raw mode");
+                    return true;
+                }
+                KeyCode::Char('n') | KeyCode::Char('N') | KeyCode::Char('ㅜ') | KeyCode::Esc => {
+                    disable_raw_mode().expect("Failed to disable raw mode");
+                    return false;
+                }
+                _ => {}
             }
-            Key::Char('n') | Key::Char('N') | Key::Char('ㅜ') | Key::Esc => {
-                disable_raw_mode().expect("Failed to disable raw mode");
-                return false;
-            }
-            _ => {}
         }
     }
-    unreachable!();
 }
 
 pub fn get_mq_input() -> bool {
     enable_raw_mode().expect("Failed to enable raw mode");
     print_mq_prompt().expect("Failed to print prompt");
 
-    for key in io::stdin().keys() {
-        match key.unwrap() {
-            Key::Char('m') | Key::Char('M') | Key::Char('ㅡ') => {
-                disable_raw_mode().expect("Failed to disable raw mode");
-                return true;
+    loop {
+        if let Ok(Event::Key(event)) = read() {
+            match event.code {
+                KeyCode::Char('m') | KeyCode::Char('M') | KeyCode::Char('ㅡ') => {
+                    disable_raw_mode().expect("Failed to disable raw mode");
+                    return true;
+                }
+                KeyCode::Char('q') | KeyCode::Char('Q') | KeyCode::Char('ㅂ') | KeyCode::Esc => {
+                    disable_raw_mode().expect("Failed to disable raw mode");
+                    return false;
+                }
+                _ => {}
             }
-            Key::Char('q') | Key::Char('Q') | Key::Char('ㅂ') | Key::Esc => {
-                disable_raw_mode().expect("Failed to disable raw mode");
-                return false;
-            }
-            _ => {}
         }
     }
-    unreachable!();
 }
 
 pub fn print_yn_prompt() -> io::Result<()> {
-    print!("{}", cursor::Save);
-    print!("{}", cursor::Goto(1, termion::terminal_size().unwrap().1));
-    print!("{}", cursor::Restore);
+    print!("{}", cursor::SavePosition);
+    print!("{}", cursor::MoveTo(1, crossterm::terminal::size()?.1));
+    print!("{}", cursor::RestorePosition);
     io::stdout().flush()?;
     Ok(())
 }
 
 pub fn print_mq_prompt() -> io::Result<()> {
-    print!("{}", cursor::Save);
-    print!("{}", cursor::Goto(1, termion::terminal_size().unwrap().1));
+    print!("{}", cursor::SavePosition);
+    print!(
+        "{}",
+        cursor::MoveTo(1, crossterm::terminal::size().unwrap().1)
+    );
     println!("PRESS 'm' to move entries or 'q' to quit...");
-    print!("{}", cursor::Restore);
+    print!("{}", cursor::RestorePosition);
     io::stdout().flush()?;
     Ok(())
 }
